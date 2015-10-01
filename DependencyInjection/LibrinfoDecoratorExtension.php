f3ce1320fba50f565c33e9b179f424d16b57d972
Baptiste SIMON <beta@e-glop.net>
2015-10-01 16:41:52 +0200

    adding the basic configuration layout... situated in ```Resources/config/decorator.yml```

--- DependencyInjection/LibrinfoDecoratorExtension.php
+++ DependencyInjection/LibrinfoDecoratorExtension.php
@@ -24,5 +24,20 @@ class LibrinfoDecoratorExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('decorator.yml');
+
+        // specialize things for easy deployments about templates/layouts, giving the possibility to personalize stuff in the app/config/config.$
+        $templates = $container->getParameter('sonata.admin.configuration.templates');
+        $decorator = is_array($container->getParameter('librinfo_decorator')) ? $container->getParameter('librinfo_decorator') : array();
+        foreach ( $decorator['templates'] as $key => $change )
+        {
+            if ( !isset($templates[$key]) || !is_array($change['original']) || !isset($change['modified']) )
+                continue;
+            if ( in_array($templates[$key], $change['original']) )
+                $templates[$key] = $change['modified'];
+                
+            echo "$key: ".$change['modified'];
+        }
+        $container->setParameter('sonata.admin.configuration.templates', $templates);
     }
 }
