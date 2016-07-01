4b615bbd28d91d91bea016abac17303ba3be5646
Romain Sanchez <romainsancho@gmail.com>
2016-07-01 17:49:56 +0200

    added margin for inline form fields

--- Resources/public/js/inline-block.js
+++ Resources/public/js/inline-block.js
@@ -1,5 +1,11 @@
-// display several sonata form inputs on the same line
+// display several sonata form fields on the same line
 $(document).ready(function(){
             
-    $('.inline-block').closest('div.form-group').css('display', 'inline-block');
+    $('.inline-block')
+        .closest('div.form-group')
+        .css({
+            'display': 'inline-block',
+            'margin-right': '10px'
+        })
+    ;
 });
