ea1ffba895692bfe044a8930d237ee07d3821e11
Romain Sanchez <romainsancho@gmail.com>
2016-07-01 15:09:18 +0200

    added script to display several form fields on the same line

---
+++ Resources/public/js/inline-block.js
@@ -0,0 +1,5 @@
+// display several sonata form inputs on the same line
+$(document).ready(function(){
+            
+    $('input.inline-block').parent().parent().css('display', 'inline-block');
+});    
