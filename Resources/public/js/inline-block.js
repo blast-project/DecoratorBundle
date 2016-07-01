f243b8d3f4f4130bc6277c9121480b9b68cecbf0
Romain Sanchez <romainsancho@gmail.com>
2016-07-01 16:50:44 +0200

    support inline form fields for choice type

--- Resources/public/js/inline-block.js
+++ Resources/public/js/inline-block.js
@@ -1,5 +1,16 @@
 // display several sonata form inputs on the same line
 $(document).ready(function(){
             
-    $('input.inline-block').parent().parent().css('display', 'inline-block');
-});    
+    $('input.inline-block, div.inline-block')
+        .parent()
+        .parent()
+        .css('display', 'inline-block')
+    ;
+    $('div.inline-block')
+        .parent()
+        .parent()
+        .parent()
+        .parent()
+        .css('display', 'inline-block')
+    ;
+});
