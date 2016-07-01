2fda334e18d1b90dce2db94c617cdb0c8f93d6d9
Romain Sanchez <romainsancho@gmail.com>
2016-07-01 17:04:16 +0200

    improved selector

--- Resources/public/js/inline-block.js
+++ Resources/public/js/inline-block.js
@@ -1,16 +1,5 @@
 // display several sonata form inputs on the same line
 $(document).ready(function(){
             
-    $('input.inline-block, div.inline-block')
-        .parent()
-        .parent()
-        .css('display', 'inline-block')
-    ;
-    $('div.inline-block')
-        .parent()
-        .parent()
-        .parent()
-        .parent()
-        .css('display', 'inline-block')
-    ;
+    $('.inline-block').closest('div.form-group').css('display', 'inline-block');
 });
