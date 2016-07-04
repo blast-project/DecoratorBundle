c07ed0cdace954fa48633f4529ccd6bdf3dd7faf
Romain Sanchez <romainsancho@gmail.com>
2016-07-04 11:17:12 +0200

    inline fields with variety descriptions

--- Resources/public/js/inline-block.js
+++ Resources/public/js/inline-block.js
@@ -1,11 +1,15 @@
 // display several sonata form fields on the same line
 $(document).ready(function(){
-            
-    $('.inline-block')
-        .closest('div.form-group')
-        .css({
-            'display': 'inline-block',
-            'margin-right': '10px'
-        })
-    ;
+    
+    $('.inline-block').each(function(){
+        
+        var formGroup = $(this).closest('div.form-group').parent().closest('div.form-group');
+        var css = {'display': 'inline-block', 'margin-right': '10px'};
+        
+        if( formGroup.length === 0){
+            $(this).closest('div.form-group').css(css);
+        }else{
+            formGroup.css(css);    
+        }
+    });
 });
